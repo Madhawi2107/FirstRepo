7d91e93042387eb6f70997b83844d5a7d03bc2d8
Madhawi2107 <95469996+Madhawi2107@users.noreply.github.com>
2021-12-03 13:59:37 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,18 @@
+// Non-Premitive::functions , Arrays , Objects
+//fn def
+function sayHi(Para)
+{
+    //console.log("say hi");
+    return "returned from a function";
+}
+//fn call
+//sayHi(10);
+//sayHi("Hello");
+let rVal=sayHi([1,2,3,4,5]);
+console.log("rval",rVal);
+
+//objects: group of key value pair
+
+
+
+
---
+++ intro.js
@@ -0,0 +1,21 @@
+//print
+console.log("Hello JS");
+//declare
+let a;
+// value in a is undefined
+//js only teels that it is a variable not the type(we can use it as any type as per our requirement)
+console.log(a);
+a=10;
+a=10.1;
+a="String";
+a="string 2";
+console.log("variable contains",a);
+//variable types:premitive types: number(either int or float),string(nothing is char),boolean,null
+
+//loop
+for(let i=1;i<=10;i++) 
+{
+    console.log("number is",i);
+}
+
+
